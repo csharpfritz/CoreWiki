383fec702520e510e8f75b970f7c7b43bde65f4e
Anthony Conrad <parithon@outlook.com>
2018-06-03 17:32:20 -0700

    Updated estimated read time to use moment.js

--- CoreWiki/Helpers/StringHelpers.cs
+++ CoreWiki/Helpers/StringHelpers.cs
@@ -1,4 +1,7 @@
-﻿namespace CoreWiki.Helpers
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CoreWiki.Helpers
 {
 	public static class StringHelpers
 	{
@@ -14,6 +17,31 @@
 		{
 			var noun = count == 1 ? singular : plural;
 			return prependCount ? $"{count} {noun}" : noun;
+		}
+		// <summary>
+		/// Returns the number of words in a string
+		/// </summary>
+		/// <param name="content">The string we wish to count the number of words contained within.</param>
+		/// <returns>The number of words in the sentance</returns>
+		public static int WordCount(this string content)
+		{
+			if (string.IsNullOrWhiteSpace(content))
+				return 0;
+
+			var matches = Regex.Matches(content, @"\b\S+\b");
+			return matches.Count;
+		}
+		/// <summary>
+		/// Returns the amount of time to read a string
+		/// </summary>
+		/// <param name="content">The string we wish to calculate.</param>
+		/// <returns>A TimeSpan of time required to read the string</returns>
+		public static TimeSpan CalculateReadTime(this string content)
+		{
+			const decimal wpm = 275.0m;
+			var wordCount = content.WordCount();
+			var minutes = (double)(wordCount / wpm);
+			return TimeSpan.FromMinutes(minutes);
 		}
 	}
 }
