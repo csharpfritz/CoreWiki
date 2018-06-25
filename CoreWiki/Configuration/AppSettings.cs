41948cf3a4f2fa39649813b69622056957004135
Ashley Broughton <a.broughton.en@gmail.com>
2018-06-25 15:36:04 +0800

    Prepare for content security policy implementation
    Awaiting on https://github.com/madskristensen/WebEssentials.AspNetCore.ServiceWorker/issues/30

--- CoreWiki/Configuration/AppSettings.cs
+++ CoreWiki/Configuration/AppSettings.cs
@@ -11,7 +11,19 @@ namespace CoreWiki.Configuration
 		public Uri Url { get; set; }
 		public Connectionstrings ConnectionStrings { get; set; }
 		public Comments Comments { get; set; }
+		public CspSettings CspSettings { get; set; }
 
+	}
+
+	public class CspSettings
+	{
+		public string[] ImageSources { get; set; }
+		public string[] StyleSources { get; set; }
+		public string[] ScriptSources { get; set; }
+		public string[] FontSources { get; set; }
+		public string[] FormActions { get; set; }
+		public string[] FrameAncestors { get; set; }
+		public string[] ReportUris { get; set; }
 	}
 
 	public class Connectionstrings
