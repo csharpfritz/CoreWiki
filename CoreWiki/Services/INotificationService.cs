ae8272385f7cc48f3bfe30e82a83fd5b690b95e5
Erwin de Vreugd <erwindevreugd@users.noreply.github.com>
2018-06-28 16:56:20 +0200

    Expended on email notification featuer and added email templates, closes #149 (#157)

---
+++ CoreWiki/Services/INotificationService.cs
@@ -0,0 +1,11 @@
+﻿using CoreWiki.Areas.Identity.Data;
+using CoreWiki.Models;
+using System.Threading.Tasks;
+
+namespace CoreWiki.Services
+{
+	public interface INotificationService
+    {
+		Task<bool> NotifyAuthorNewComment(CoreWikiUser author, Article article, Comment comment);
+	}
+}
