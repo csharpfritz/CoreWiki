5e2d17d2f516355b23d84de36af83cce42a07a50
Ashley Broughton <a.broughton.en@gmail.com>
2018-06-28 14:51:09 +0800

    Remove unsafe elements and attributes

--- CoreWiki/TagHelpers/MarkdownTagHelper.cs
+++ CoreWiki/TagHelpers/MarkdownTagHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using Ganss.XSS;
@@ -26,13 +28,50 @@ namespace CoreWiki.TagHelpers
 			output.Content.SetHtmlContent(content);
 		}
 
-		public string ConvertMarkdown(string unsafeMarkdown)
+		public string ConvertMarkdown(string markdown)
 		{
-			var encodedMarkdown = HttpUtility.HtmlEncode(unsafeMarkdown);
-			var unsafeContent = Westwind.AspNetCore.Markdown.Markdown.Parse(encodedMarkdown);
-			var safeContent = new HtmlSanitizer().Sanitize(unsafeContent);
+			var sanitizer = new HtmlSanitizer();
+			sanitizer.RemovingAttribute += OnRemovingAttribute;
 
-			return safeContent;
+			// disallow form elements
+			sanitizer.AllowedTags.Remove("form");
+			sanitizer.AllowedTags.Remove("button");
+			sanitizer.AllowedTags.Remove("input");
+
+			// parse the markdown to HTML
+			markdown = Westwind.AspNetCore.Markdown.Markdown.Parse(markdown);
+
+			// encode any custom tags (pasting e-mails and other markup (XML))
+			// From author of HtmlSanitizer
+			// https://gist.github.com/mganss/00bec2c2245c0ef86d9c82d6211def7b
+			markdown = HtmlRegex.Replace(markdown, m =>
+			{
+				var tagName = m.Groups[1].Value;
+				if (!HtmlTags.Contains(tagName))
+					return "&lt;" + m.Value.Substring(1);
+				return m.Value;
+			});
+
+			markdown = sanitizer.Sanitize(markdown);
+
+			return markdown;
 		}
+
+		private void OnRemovingAttribute(object sender, RemovingAttributeEventArgs e)
+		{
+			if (!e.Attribute.Value.Contains("vbscript:", StringComparison.CurrentCultureIgnoreCase)
+				&& !e.Attribute.Value.Contains("javascript:", StringComparison.CurrentCultureIgnoreCase)
+				&& !e.Attribute.Value.Contains("data:", StringComparison.CurrentCultureIgnoreCase)
+				&& !e.Attribute.Name.StartsWith("on", StringComparison.CurrentCultureIgnoreCase))
+			{
+				// don't remove the attribute if we've deemed it safe
+				e.Cancel = true;
+			}
+		}
+
+		// From author of HtmlSanitizer
+		// https://gist.github.com/mganss/00bec2c2245c0ef86d9c82d6211def7b
+		static Regex HtmlRegex = new Regex(@"</?([a-z]+[1-6]?)", RegexOptions.IgnoreCase);
+		static HashSet<string> HtmlTags = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { "a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "datalist", "dd", "del", "details", "dfn", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "isindex", "kbd", "keygen", "label", "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr" };
 	}
 }
