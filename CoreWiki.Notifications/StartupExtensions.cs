e448812b3a0749bb731f512a2853a1330608ad0b
Erwin de Vreugd <erwindevreugd@casema.nl>
2018-07-12 20:55:17 +0200

    Added razor view engine options to service collection

--- CoreWiki.Notifications/StartupExtensions.cs
+++ CoreWiki.Notifications/StartupExtensions.cs
@@ -3,7 +3,10 @@ using CoreWiki.Core.Notifications;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.FileProviders;
 
 namespace CoreWiki.Notifications
 {
@@ -17,6 +20,11 @@ namespace CoreWiki.Notifications
 			services.AddScoped<IEmailMessageFormatter, EmailMessageFormatter>();
 			services.AddScoped<IEmailNotifier, EmailNotifier>();
 
+		    services.Configure<RazorViewEngineOptions>(options =>
+		    {
+                options.FileProviders.Add(
+                    new EmbeddedFileProvider(typeof(TemplateProvider).GetTypeInfo().Assembly));
+		    });
 			return services;
 
 		}
