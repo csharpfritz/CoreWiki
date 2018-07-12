f7657d42d0907e535613c36e8655514306ae830f
Erwin de Vreugd <erwindevreugd@casema.nl>
2018-07-12 19:47:19 +0200

    Added email templates

---
+++ CoreWiki.Notifications/Models/NewCommentEmailModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreWiki.Notifications.Models
+{
+    public class NewCommentEmailModel
+    {
+        public string BaseUrl { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string ArticleTopic { get; set; }
+        public string ArticleUrl { get; set; }
+        public string CommenterDisplayName { get; set; }
+    }
+}
