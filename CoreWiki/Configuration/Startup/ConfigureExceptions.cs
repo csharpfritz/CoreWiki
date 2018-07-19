8b2c4f0591241e4ad4fc1ee51bb57d962d5af770
Ashley Broughton <a.broughton.en@gmail.com>
2018-07-19 22:28:48 +0800

    Extension methods for cleaner startup (#195)

---
+++ CoreWiki/Configuration/Startup/ConfigureExceptions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CoreWiki.Configuration.Startup
+{
+	public static partial class ConfigurationExtensions
+	{
+		public static IApplicationBuilder ConfigureExceptions(this IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+			}
+
+			return app;
+		}
+	}
+}
---
+++ CoreWiki/Configuration/Startup/ConfigureTelemetry.cs
@@ -0,0 +1,20 @@
+﻿using CoreWiki.Helpers;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreWiki.Configuration.Startup
+{
+	public static partial class ConfigurationExtensions
+	{
+		public static IApplicationBuilder ConfigureTelemetry(this IApplicationBuilder app)
+		{
+			var initializer = new ArticleNotFoundInitializer();
+
+			var configuration = app.ApplicationServices.GetService<TelemetryConfiguration>();
+			configuration.TelemetryInitializers.Add(initializer);
+
+			return app;
+		}
+	}
+}
