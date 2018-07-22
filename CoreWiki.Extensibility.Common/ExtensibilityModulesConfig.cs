32750d91d146adad219e0c92af5f18acc9ac3d10
Erwin de Vreugd <erwindevreugd@casema.nl>
2018-07-22 11:31:57 +0200

    Removed ExtensibilityModulesConfig poco

--- CoreWiki.Extensibility.Common/ExtensibilityModulesConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CoreWiki.Extensibility.Common
-{
-    public class ExtensibilityModulesConfig
-    {
-        public string Name { get; set; }
-        public string Type { get; set; }
-    }
-}
