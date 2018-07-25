4cd01eb564f8a8e5d56a3a629957a224110cb4c5
Ashley Broughton <a.broughton.en@gmail.com>
2018-07-25 09:28:10 +0800

    Introduced DTO for All articles page

---
+++ CoreWiki.Data/Models/ArticleSummaryDTO.cs
@@ -0,0 +1,12 @@
+﻿using NodaTime;
+
+namespace CoreWiki.Data.Models
+{
+	public class ArticleSummaryDTO
+	{
+		public string Slug { get; set; }
+		public string Topic { get; set; }
+		public Instant Published { get; set; }
+		public int ViewCount { get; set; }
+	}
+}
