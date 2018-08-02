1d081c15ff9f608bd3e4c6eb15c84abbeb3a1dce
Ashley Broughton <a.broughton.en@gmail.com>
2018-08-02 11:04:40 +0800

    Introduced DTO for Edit page + refactoring
    Makes sure topic doesn't conflict with existing article
    Redirect back to view page if nothing has changed (Fixes #222)
    Discovered #239 during testing

---
+++ CoreWiki.Data/Models/ArticleEditDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreWiki.Data.Models
+{
+	public class ArticleEditDto
+	{
+		public int Id { get; set; }
+		public string Topic { get; set; }
+		public string Content { get; set; }
+	}
+}
