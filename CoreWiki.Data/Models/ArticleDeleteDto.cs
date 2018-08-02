96c1883c53f70dab95e2727d085729ee62e83614
Ashley Broughton <a.broughton.en@gmail.com>
2018-08-02 09:38:54 +0800

    Introduced DTO for Delete page

---
+++ CoreWiki.Data/Models/ArticleDeleteDto.cs
@@ -0,0 +1,11 @@
+﻿using NodaTime;
+
+namespace CoreWiki.Data.Models
+{
+	public class ArticleDeleteDto
+	{
+		public string Topic { get; set; }
+		public string Content { get; set; }
+		public Instant Published { get; set; }
+	}
+}
