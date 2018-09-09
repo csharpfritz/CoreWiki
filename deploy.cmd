25ad0791a01b46a4787eeb02d30de3f53b7e7a5a
Frank Boucher <fboucher@outlook.com>
2018-09-09 11:31:21 -0400

    Change the initial task on Azure

--- deploy.cmd
+++ deploy.cmd
@@ -2,4 +2,4 @@
 IF NOT EXIST "Tools" (md "Tools")
 IF NOT EXIST "Tools\Addins" (md "Tools\Addins")
 nuget install Cake -ExcludeVersion -OutputDirectory "Tools"
-.\Tools\Cake\Cake.exe build.cake -verbosity=Verbose
+.\Tools\Cake\Cake.exe build.cake --target="Kudu-Sync" -verbosity=Verbose
