76b886d4947132340c8db1d38a7c6f084e6f4294
Frank Boucher <fboucher@outlook.com>
2018-09-09 11:04:11 -0400

    Create deploy.cmd

---
+++ deploy.cmd
@@ -0,0 +1,5 @@
+@echo off
+IF NOT EXIST "Tools" (md "Tools")
+IF NOT EXIST "Tools\Addins" (md "Tools\Addins")
+nuget install Cake -ExcludeVersion -OutputDirectory "Tools"
+.\Tools\Cake\Cake.exe build.cake -verbosity=Verbose
