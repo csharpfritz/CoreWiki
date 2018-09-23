9c1f85e4a83424e0559ed3e273213a3722e377ca
Unknown <argtang@outlook.com>
2018-09-23 14:48:30 +0200

    Found Azure Search bug?

--- CoreWiki.Application/Articles/Search/Impl/AzureSearch/AzureSearchClient.cs
+++ CoreWiki.Application/Articles/Search/Impl/AzureSearch/AzureSearchClient.cs
@@ -10,9 +10,13 @@ namespace CoreWiki.Application.Articles.Search.AzureSearch
 		private readonly IOptionsSnapshot<SearchProviderSettings> _config;
 		private readonly IConfiguration _configuration;
 
-		private string searchServiceName => _config.Value.Az_ApiGateway; //_configuration["SearchProvider:Az_ApiGateway"];
-		private string adminApiKey => _config.Value.Az_ReadApiKey; //_configuration["SearchProvider:Az_ReadApiKey"];
-		private string queryApiKey => _config.Value.Az_WriteApiKey; //_configuration["SearchProvider:Az_WriteApiKey"];
+		private string SearchServiceName => _config.Value.Az_ApiGateway;
+		private string AdminApiKey => _config.Value.Az_WriteApiKey;
+		private string QueryApiKey => _config.Value.Az_ReadApiKey;
+		private string GetIndexName<T>()
+		{
+			return typeof(T).Name.ToLowerInvariant();
+		}
 
 		public AzureSearchClient(IOptionsSnapshot<SearchProviderSettings> config, IConfiguration configuration)
 		{
@@ -23,33 +27,44 @@ namespace CoreWiki.Application.Articles.Search.AzureSearch
 		public ISearchIndexClient CreateServiceClient<T>()
 		{
 			var index = typeof(T).FullName;
-			var serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
+			var serviceClient = new SearchServiceClient(SearchServiceName, new SearchCredentials(AdminApiKey));
 			return GetOrCreateIndex<T>(serviceClient);
 		}
 
 		private ISearchIndexClient GetOrCreateIndex<T>(SearchServiceClient serviceClient)
 		{
-			var index = typeof(T).FullName;
+			//indexname must be lowercase
+			var index = GetIndexName<T>();
 			if (serviceClient.Indexes.Exists(index))
 			{
 				return serviceClient.Indexes.GetClient(index);
 			}
 
-			var definition = new Index()
-			{
-				Name = index,
-				Fields = FieldBuilder.BuildForType<T>()
-			};
 
-			serviceClient.Indexes.Create(definition);
+			try
+			{
+				var definition = new Index()
+				{
+					Name = index,
+					// NodaTimecrash
+					Fields = FieldBuilder.BuildForType<T>()
+				};
+				var createdindex = serviceClient.Indexes.Create(definition);
+			}
+			catch (System.Exception e)
+			{
+				throw;
+			}
 
 			return serviceClient.Indexes.GetClient(index);
 		}
 
 		public ISearchIndexClient GetSearchClient<T>()
 		{
-			var indexClient = new SearchIndexClient(searchServiceName, typeof(T).FullName, new SearchCredentials(queryApiKey));
+			var indexClient = new SearchIndexClient(SearchServiceName, GetIndexName<T>(), new SearchCredentials(QueryApiKey));
 			return indexClient;
 		}
+
+
 	}
 }
